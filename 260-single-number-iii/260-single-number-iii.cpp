4a7a59a0b58ff4b1f372cb6ea749c7bb0674907d
Paramarthi Phukan <88555057+paramarthiphukan@users.noreply.github.com>
2022-08-17 16:39:28 +0530

    Time: 4 ms (98.00%), Space: 9.9 MB (92.81%) - LeetHub

---
+++ 260-single-number-iii/260-single-number-iii.cpp
@@ -0,0 +1,17 @@
+class Solution 
+{
+public:
+    vector<int> singleNumber(vector<int>& nums) 
+    {
+     long long int xr=0;
+        int n1=0,n2=0;
+        for(auto& i:nums)xr^=i;
+        int rt_set_bit=xr&~(xr-1);        
+        for(auto& i:nums)
+        {
+            if(i&rt_set_bit)n1^=i;
+            else n2^=i;
+        }
+        return {n1,n2};
+    }
+};
