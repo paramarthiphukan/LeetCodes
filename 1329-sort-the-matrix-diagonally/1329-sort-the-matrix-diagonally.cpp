c835e69693fbef76635194265f3be79c93b9e0d0
Paramarthi Phukan <88555057+paramarthiphukan@users.noreply.github.com>
2022-08-28 11:48:45 +0530

    Time: 16 ms (64.04%), Space: 9.3 MB (52.67%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) 
+    vector<vector<int>> diagonalSort(vector<vector<int>>& matrix) 
     {
         int i,j,n,m,k,l;
-        n = mat.size();
-        m=mat[0].size();
+        n = matrix.size();
+        m=matrix[0].size();
         j=n-1,i=0;
         for(j=m-1;j>=0;j--)
         {
@@ -12,13 +12,13 @@ public:
             k=i; l=j;
             while(k<n && l<m)
             { 
-               a.push_back(mat[k][l]); 
+               a.push_back(matrix[k][l]); 
                 l++;k++;}
             sort(a.begin(),a.end());
             k=i;l=j;
             int o=0;
             while(k<n &&l<m && o<a.size())
-            { mat[k][l] = a[o++]; l++;k++;}
+            { matrix[k][l] = a[o++]; l++;k++;}
         }
         j=0;
          for(i=1;i<n;i++)
@@ -27,14 +27,14 @@ public:
             k=i; l=j;
             while(k<n && l<m)
             { 
-               a.push_back(mat[k][l]); 
+               a.push_back(matrix[k][l]); 
                 l++;k++;}
             sort(a.begin(),a.end());
             k=i;l=j;
             int o=0;
             while(k<n &&l<m && o<a.size())
-            { mat[k][l] = a[o++]; l++;k++;}
+            { matrix[k][l] = a[o++]; l++;k++;}
         }
-        return mat;
+        return matrix;
     }
 };
