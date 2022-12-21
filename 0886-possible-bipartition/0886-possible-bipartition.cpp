b3979658c62aa510a22058073b80588948765e54
Paramarthi Phukan <88555057+paramarthiphukan@users.noreply.github.com>
2022-12-21 13:35:55 +0530

    Time: 203 ms (96.44%), Space: 64 MB (90.89%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.cpp
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
-    bool dfs( vector<int> adj[],vector<int> &color,int node){
+    bool dfs( vector<int> adj[],vector<int> &color,int node)
+    {
         bool ans=true;
-        for(auto i: adj[node]){
-            if(color[i]==-1){
+        for(auto i: adj[node])
+        {
+            if(color[i]==-1)
+            {
                 color[i]=1-color[node];
                 ans=ans and dfs(adj,color,i);
             }
-            else{
+            else
+            {
                 if(color[i]==color[node]) return false;
             }
         }
         return ans;
     }
-    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes)
+    {
         vector<int> adj[n+1];
-        for(int i=0;i<dislikes.size();i++){
+        for(int i=0;i<dislikes.size();i++)
+        {
             adj[dislikes[i][0]].push_back(dislikes[i][1]);
             adj[dislikes[i][1]].push_back(dislikes[i][0]);
         }
         vector<int> color(n+1,-1);
         bool b=true;
-        for(int i=1;i<=n;i++){
-            if(color[i]==-1){
+        for(int i=1;i<=n;i++)
+        {
+            if(color[i]==-1)
+            {
                 color[i]=1;
                 b=b and dfs(adj,color,i);
             }
