509317920cc3bd50a752ebf508c01c9ad5f63d92
Paramarthi Phukan <88555057+paramarthiphukan@users.noreply.github.com>
2022-12-29 12:06:41 +0530

    Time: 1741 ms (14.54%), Space: 203 MB (5.02%) - LeetHub

---
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+public:
+    vector<int> res;
+    vector<int> getOrder(vector<vector<int>>& tasks) 
+    {
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> valid;
+        for(int i=0; i<tasks.size(); i++)
+        {
+            pq.push({tasks[i][0], tasks[i][1], i});
+        }
+        long long timestamp = pq.top()[0];
+
+        while(!pq.empty() || !valid.empty())
+        {
+            while(!pq.empty() && timestamp>=pq.top()[0])
+            { 
+                auto curr = pq.top();
+                pq.pop();
+                valid.push({curr[1], curr[2]});
+            }
+            if(valid.empty())
+            {
+                timestamp = pq.top()[0];
+                continue;
+            }
+            auto validTop = valid.top();
+            valid.pop();
+            res.push_back(validTop[1]);
+            timestamp+=validTop[0];
+        }
+        
+        return res;
+
+    }
+};
