ec560f962a7aa2b10c254bba86053bc44634b3e1
Paramarthi Phukan <88555057+paramarthiphukan@users.noreply.github.com>
2023-07-26 10:55:14 +0530

    Added solution - LeetHub

---
+++ Kth Ancestor in a Tree - GFG/kth-ancestor-in-a-tree.cpp
@@ -0,0 +1,145 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+int kthAncestor(Node *root, int k, int node);
+
+int main()
+{
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        int k , node;
+        scanf("%d ",&k);
+        scanf("%d ",&node);
+        string s;
+		getline(cin,s);
+		Node* root = buildTree(s);
+		cout<<kthAncestor(root,k,node)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+//User function Template for C++
+/*
+Structure of the node of the binary tree is as
+struct Node
+{
+	int data;
+	struct Node *left, *right;
+};
+*/
+// your task is to complete this function
+int const N=1e5+2;
+vector<int> v(N, 1);
+void dfs(Node *vertex, Node *parent, vector<int> &nodes)
+{
+    if (vertex->data==1) v[1]=0;
+    else v[vertex->data] = parent->data; 
+    nodes.push_back(vertex->data);
+    if (vertex->left!=nullptr)
+    {
+        dfs(vertex->left, vertex, nodes);
+    }
+    if (vertex->right!=nullptr)
+    {
+        dfs(vertex->right, vertex, nodes);
+    }
+}
+int kthAncestor(Node *root, int k, int node)
+{
+   
+    vector<int> nodes;
+    dfs(root, root, nodes);
+    vector<int> p(nodes.size()+1, 0);
+    for(auto val:nodes) p[val] = v[val];
+    v.clear();
+    while(node!=0)
+    {
+        v.push_back(node);
+        node = p[node];
+    }
+    if (k>v.size()-1) return -1;
+    return v[k];
+}
