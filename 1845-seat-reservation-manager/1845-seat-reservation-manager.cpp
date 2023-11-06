5d8f6486e9c0ef43db2ba164798687a2ad91991c
Paramarthi Phukan <88555057+paramarthiphukan@users.noreply.github.com>
2023-11-06 20:45:43 +0530

    Time: 253 ms (98.28%), Space: 146.8 MB (91.10%) - LeetHub

---
+++ 1845-seat-reservation-manager/1845-seat-reservation-manager.cpp
@@ -0,0 +1,32 @@
+class SeatManager {
+public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    vector<int> vis;
+    vector<int> v;
+    SeatManager(int n) {
+        v.resize(n + 2, 0);
+        vis.resize(n + 2, 0);
+        pq.push(1);
+        vis[1] = 1;
+    }
+    
+    int reserve() {
+        int ans = pq.top();
+        pq.pop();
+        vis[ans] = 0;
+        v[ans++] = 1;
+        if(!v[ans] && !vis[ans]){
+            pq.push(ans);
+            vis[ans] = 1;
+        }
+        return ans - 1;
+    }
+    
+    void unreserve(int seatNumber) {
+        v[seatNumber] = 0;
+        if(!vis[seatNumber]){
+            pq.push(seatNumber);
+            vis[seatNumber] = 1;
+        }
+    }
+};
