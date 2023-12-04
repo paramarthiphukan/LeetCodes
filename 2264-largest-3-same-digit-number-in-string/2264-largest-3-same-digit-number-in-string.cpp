ab4030ae5de6c85938ce4c1ec02c39e8b99c8c9b
Paramarthi Phukan <88555057+paramarthiphukan@users.noreply.github.com>
2023-12-05 00:59:24 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (91.88%) - LeetHub

---
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    // time/space: O(n)/O(1)
+    string largestGoodInteger(string num) {
+        int n = num.size();
+        string result = "";
+        for (int i = 0; i < n; i++) {
+            if (!isGood(num, i)) continue;
+            if ((result == "") || (num[i] > result[0])) result = num.substr(i, 3);\
+        }
+        return result;
+    }
+private:
+    bool isGood(const string& num, int i) {
+        // base case
+        if ((i + 2) >= num.size()) return false;
+
+        // check the substring consists of only one unique digit
+        return ((num[i] == num[i + 1]) && (num[i] == num[i + 2]));
+    }
+};
